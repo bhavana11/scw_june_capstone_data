47822458ec70f913ff9467edfb4c435c64b156d9
Bhavana Sherchand <bhavana@vm64-42.iplantcollaborative.org>
2016-06-12 16:27:59 -0700

    Adding Python Code

--- capstone.py
+++ capstone.py
@@ -1,5 +1,9 @@
-
+......................
 Authors:Test1 and Test2
 this script makes plots for the capstone
 I'm adding text 
+.....................
+import pandas as pd
+from matplotlib import pyplot as plt
+dat=pd.read_csv("data/gapminder-FiveYearData.csv")
 
